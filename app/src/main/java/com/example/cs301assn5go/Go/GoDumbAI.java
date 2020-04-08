d588d9547ffaf183143de556bb4691abaceac6ea
Zhi Zun Chen <beurryllmew@gmail.com>
2020-04-08 11:44:37 -0700

    Reverted to old algorithm

--- app/src/main/java/com/example/cs301assn5go/Go/GoDumbAI.java
+++ app/src/main/java/com/example/cs301assn5go/Go/GoDumbAI.java
@@ -19,7 +19,7 @@ public class GoDumbAI extends GameComputerPlayer {
     protected void receiveInfo(GameInfo info) {
         if (info instanceof NotYourTurnInfo) return;
         // Accepts info
-            //int[][] board = ((GoState) info).getBoard(); // Local understanding of board
+            int[][] board = ((GoState) info).getBoard(); // Local understanding of board
             pass += 0.02;
 
         // Chooses a random move
@@ -27,7 +27,7 @@ public class GoDumbAI extends GameComputerPlayer {
             game.sendAction(new GoPassAction(this));
         } else {
             int[] choice;
-            choice = random();
+            choice = random(board);
             int x = choice[0];
             int y = choice[1];
             game.sendAction(new GoMoveAction(this, x, y));
@@ -37,30 +37,24 @@ public class GoDumbAI extends GameComputerPlayer {
     /** random: chooses a random position on the board
      *
      */
-    private int[] random() {
-        int size = 13;
+    private int[] random(int[][] board) {
+        int size = board.length;
         Random rand = new Random();
-        int x = rand.nextInt(size);
-        int y = rand.nextInt(size);
+        int num = rand.nextInt(size*size);
 
-        int[] info = new int[2];
-        info[0] = x;
-        info[1] = y;
-        return info;
-
-//        for (int x = 0; x < size; x++ ) {
-//            for (int y = 0; y < size; y++) {
-//                if (board[x][y] == 2) {
-//                    num --;
-//                    if (num == 0) {
-//                        int[] info = new int[2];
-//                        info[0] = x;
-//                        info[1] = y;
-//                        return info;
-//                    }
-//                }
-//            }
-//        }
-//        return new int[]{-1,-1};
+        for (int x = 0; x < size; x++ ) {
+            for (int y = 0; y < size; y++) {
+                if (board[x][y] == 2) {
+                    num --;
+                    if (num == 0) {
+                        int[] info = new int[2];
+                        info[0] = x;
+                        info[1] = y;
+                        return info;
+                    }
+                }
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
