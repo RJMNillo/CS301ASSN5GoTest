d62de40dab296020a61db3b06696fe5af59d80d3
VandanBh <59784835+VandanBh@users.noreply.github.com>
2020-04-07 18:03:18 -0700

    Continued connecting few classes and started View

--- app/src/main/java/com/example/cs301assn5go/Go/GoView.java
+++ app/src/main/java/com/example/cs301assn5go/Go/GoView.java
@@ -1,6 +1,8 @@
 package com.example.cs301assn5go.Go;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.util.AttributeSet;
 
 import com.example.cs301assn5go.game.GameFramework.GameFramework.utilities.FlashSurfaceView;
@@ -10,7 +12,26 @@ import com.example.cs301assn5go.game.GameFramework.GameFramework.utilities.Flash
  */
 
 public class GoView extends FlashSurfaceView {
+    protected GoState state;
     public GoView(Context context) {
         super(context);
+    }
+
+    public void onDraw(Canvas c){
+        //drawBoard(c);
+        //drawPieces(c, state);
+    }
+    public void drawBoard(Canvas c){
+
+    }
+    public void drawPieces(Canvas C, GoState s){
+        int[][] board = s.getBoard();
+
+        //draw through mapPieces(x, y)
+    }
+
+    public Point mapPieces(int xPos, int yPos){
+        //translates board pos to draw coordinate
+        return new Point();
     }
 }
