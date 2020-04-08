c1be90c803b7bce53dfec35d8dfc5c75ae46decd
Zhi Zun Chen <beurryllmew@gmail.com>
2020-04-08 13:29:24 -0700

    SmartAI Test is working, but not very smart

--- app/src/main/java/com/example/cs301assn5go/Go/GoSmartAI.java
+++ app/src/main/java/com/example/cs301assn5go/Go/GoSmartAI.java
@@ -1,5 +1,7 @@
 package com.example.cs301assn5go.Go;
 
+import android.util.Log;
+
 import com.example.cs301assn5go.game.GameFramework.GameFramework.GameComputerPlayer;
 import com.example.cs301assn5go.game.GameFramework.GameFramework.infoMessage.GameInfo;
 
@@ -21,16 +23,28 @@ public class GoSmartAI extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
+        // Creates Instances
+        int[][] board;
+
         // Accepts info
-        int[][] board; // Local understanding of board
-        pass += 0.02;
+        if (info instanceof GoState) {
+            board = ((GoState) info).getBoard(); // Local understanding of board
+        } else {
+            return;
+        }
+
+        // Creates Valuemap
+        float[][] value = valuemap(board);
+
+        // Makes move
+        choose(value);
     }
 
     /** random: chooses a random position on the board
      *
      * @param input board of the gameState
      */
-    private float[][] valuemap(float[][] input) {
+    private float[][] valuemap(int[][] input) {
         float[][] output = new float[input.length][input.length];
         int total = 0;
         for (int x1 = 0; x1 < input.length; x1++) {
@@ -38,23 +52,59 @@ public class GoSmartAI extends GameComputerPlayer {
                 if (input[x1][y1] != 0) {
                     total ++;
                 }
+                output[x1][y1] = 0;
             }
         }
 
         for (int x1 = 0; x1 < input.length; x1++) {
             for (int y1 = 0; y1 < input.length; y1++) {
-                for (int x2 = 0; x2 < input.length; x2++) {
-                    for (int y2 = 0; y2 < input.length; y2++) {
-                        float effect = 1/((float)Math.abs(y2+x2-y1-x1)) / total;
-                        if (input[x2][y2] == mypeice) {
-                            output[x1][y1] = effect;
-                        } else {
-                            output[x1][y1] = -effect;
+                if (input[x1][y1] == 2) {
+                    for (int x2 = 0; x2 < input.length; x2++) {
+                        for (int y2 = 0; y2 < input.length; y2++) {
+                            float effect = (float)(3.5 - Math.abs(y2 - y1) - Math.abs(x2 - x1));
+                            if (input[x2][y2] == mypeice) {
+                                output[x1][y1] -= effect;
+                            } else {
+                                output[x1][y1] += effect;
+                            }
                         }
                     }
+                } else {
+                    output[x1][y1] = -100000;
                 }
             }
         }
         return output;
+    }
+
+    /** choose: chooses the highest value position on the board
+     *
+     * @param input board of the gameState
+     */
+    private void choose(float[][] input) {
+        Log.d("SmartAI","Choosing...");
+        float max = -100000;
+        int x_max = -1;
+        int y_max = -1;
+
+        // Finds the highest value
+        for (int x = 0; x < input.length; x ++) {
+            for (int y = 0; y < input.length; y ++) {
+                if (input[x][y] > max) {
+                    max = input[x][y];
+                    x_max = x;
+                    y_max = y;
+                }
+            }
+        }
+        Log.d("SmartAI","Max: " + max);
+        Log.d("SmartAI","x:" + x_max);
+        Log.d("SmartAI","y:" + y_max);
+        if (max > pass) {
+            game.sendAction(new GoMoveAction(this, x_max, y_max));
+        } else {
+            Log.d("SmartAI","Pass");
+            game.sendAction(new GoPassAction(this));
+        }
     }
 }
