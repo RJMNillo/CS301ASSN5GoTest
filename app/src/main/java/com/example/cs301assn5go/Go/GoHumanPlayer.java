a0cc582bf379004133465d9ce793df8fddc70992
Braeden Lane <braedenlane10@gmail.com>
2020-04-08 01:51:51 -0700

    Added in HumanPlayer. Trying to get GUI to appear

--- app/src/main/java/com/example/cs301assn5go/Go/GoHumanPlayer.java
+++ app/src/main/java/com/example/cs301assn5go/Go/GoHumanPlayer.java
@@ -1,16 +1,41 @@
 package com.example.cs301assn5go.Go;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.example.cs301assn5go.R;
 import com.example.cs301assn5go.game.GameFramework.GameFramework.GameHumanPlayer;
 import com.example.cs301assn5go.game.GameFramework.GameFramework.GameMainActivity;
 import com.example.cs301assn5go.game.GameFramework.GameFramework.infoMessage.GameInfo;
+import com.example.cs301assn5go.game.GameFramework.GameFramework.infoMessage.IllegalMoveInfo;
+import com.example.cs301assn5go.game.GameFramework.GameFramework.infoMessage.NotYourTurnInfo;
+import com.example.cs301assn5go.game.GameFramework.GameFramework.utilities.Logger;
 
-public class GoHumanPlayer extends GameHumanPlayer implements View.OnTouchListener, View.OnClickListener
-{
+/**
+ * A GUI that allows a human to play Go. Moves are made by clicking
+ * regions on a canvas and confirming with a button
+ *
+ * @author Braeden Lane
+ * @version April 2020
+ */
+public class GoHumanPlayer extends GameHumanPlayer implements View.OnTouchListener, View.OnClickListener {
+    // Tag for logging
+    private static final String TAG = "GoHumanPlayer";
 
-    private GameMainActivity someactivity;
+    // the current activity
+    private Activity myActivity;
+
+    // the surface view
+    private GoView surfaceView;
+
+    // the state
+    private GoState state;
+
+    // the ID for the layout to use
+    private int layoutID;
+
     /**
      * constructor
      *
@@ -32,17 +57,40 @@ public class GoHumanPlayer extends GameHumanPlayer implements View.OnTouchListen
 
     @Override
     public View getTopView() {
-        return null;
+        return myActivity.findViewById(R.id.top_gui_layout);
     }
 
     @Override
     public void receiveInfo(GameInfo info) {
 
+        if(surfaceView == null) return;
+
+        if(info instanceof IllegalMoveInfo || info instanceof NotYourTurnInfo) {
+            surfaceView.flash(Color.RED, 50);
+        }
+
+        else if (!(info instanceof GoState)) return;
+
+        else {
+            state = (GoState)info;
+            surfaceView.setState(state);
+            surfaceView.invalidate();
+            Logger.log(TAG, "receiving");
+        }
     }
 
     @Override
-    public void setAsGui(GameMainActivity activity)
-    {
-        //Get the activity
+    public void setAsGui(GameMainActivity activity) {
+        // remember our activity
+        myActivity = activity;
+
+        // Load the layout resource for the new configuration
+        activity.setContentView(R.layout.activity_main);
+
+        // set the surfaceView instance variable
+        surfaceView = (GoView)myActivity.findViewById(R.id.surfaceView);
+        Logger.log("set listener", "OnTouch");
+        surfaceView.setOnTouchListener(this);
+        surfaceView.setState(state);
     }
 }
