3ef474f8ca0b4ac3457c343738110f6872e93c38
Zhi Zun Chen <beurryllmew@gmail.com>
2020-04-29 01:01:05 -0700

    Rebalanced AI algorithm and documented code

--- app/src/main/java/com/example/cs301assn5go/Go/GoDumbAI.java
+++ app/src/main/java/com/example/cs301assn5go/Go/GoDumbAI.java
@@ -15,28 +15,42 @@ import java.util.Random;
  * @author Zhi "George" Chen
  * @version April 2020
  */
-
 public class GoDumbAI extends GameComputerPlayer {
     // Instance Variables
     private float pass; // Chance to pass
 
-    // Default Constructor
+    /**
+     * Default Constructor
+     *
+     * @param name name of the AI
+     */
     public GoDumbAI(String name) {
         super(name);
     }
 
+    /**
+     * receiveInfo overriden method called upon request for a move
+     *
+     * @param info information or a copy of the gameState
+     */
     @Override
     protected void receiveInfo(GameInfo info) {
+        // Makes sure the info is the gameState
         if (info instanceof NotYourTurnInfo) return;
         if (info instanceof IllegalMoveInfo) return;
+
         // Accepts info
-            int[][] board = ((GoState) info).getBoard(); // Local understanding of board
-            pass += 0.001;
+        int[][] board = ((GoState) info).getBoard(); // Local understanding of board
+
+        // Increments chance of passing every call
+        pass += 0.001;
 
         // Chooses a random move
         if (pass >= 1) {
+            // Sends pass gameAction
             game.sendAction(new GoPassAction(this));
         } else {
+            // Sends a random move as a gameAction
             int[] choice;
             choice = random(board);
             int x = choice[0];
@@ -48,17 +62,23 @@ public class GoDumbAI extends GameComputerPlayer {
 
     /** random: chooses a random position on the board
      *
+     * @param board the board associated with the copied gameState
      */
     private int[] random(int[][] board) {
+        // Generates a random value based of the size of the board
         int size = board.length;
         Random rand = new Random();
         int num = rand.nextInt(size*size);
 
+        // Iterates through the board for that amount of times, looping if necessary
         while (num >= 0) {
             for (int x = 0; x < size; x++) {
                 for (int y = 0; y < size; y++) {
                     if (board[x][y] == 2) {
+                        // Increment
                         num--;
+
+                        // Once 0 is reached, send the current cell as the move
                         if (num == 0) {
                             int[] info = new int[2];
                             info[0] = x;
@@ -69,6 +89,8 @@ public class GoDumbAI extends GameComputerPlayer {
                 }
             }
         }
+
+        // Error catch
         return new int[]{-1,-1};
     }
 }
--- app/src/main/java/com/example/cs301assn5go/Go/GoSmartAI.java
+++ app/src/main/java/com/example/cs301assn5go/Go/GoSmartAI.java
@@ -7,7 +7,7 @@ import com.example.cs301assn5go.game.GameFramework.GameFramework.GameComputerPla
 import com.example.cs301assn5go.game.GameFramework.GameFramework.infoMessage.GameInfo;
 
 /**
- * An AI to be used in Go that makes calculated and smart decisions
+ * An AI to be used in Go that makes calculated and "smart" decisions
  *
  * @author Zhi "George" Chen
  * @version April 2020
@@ -31,9 +31,14 @@ public class GoSmartAI extends GameComputerPlayer {
         super(name);
     }
 
+    /**
+     * receiveInfo accepts info or a copy of the gameState
+     *
+     * @param info the info or copy of the gameState
+     */
     @Override
     protected void receiveInfo(GameInfo info) {
-        // Creates Instances
+        // Creates temporary instances
         int[][] board;
         mypeice = 0;
 
@@ -44,8 +49,6 @@ public class GoSmartAI extends GameComputerPlayer {
             return;
         }
 
-        pass += 0.01;
-
         // Creates Valuemap
         float[][] value = valuemap(board);
 
@@ -54,7 +57,7 @@ public class GoSmartAI extends GameComputerPlayer {
 
         // Runs algorithms
         apply_rng(choice);
-        //apply_bias(choice);
+        apply_bias(choice);
         apply_capture(choice, value, board);
         apply_protect(choice, value, board);
         apply_occupied(choice, board, memory);
@@ -63,9 +66,11 @@ public class GoSmartAI extends GameComputerPlayer {
         choose(choice);
     }
 
-    /** random: chooses a random position on the board
+    /**
+     * valuemap: chooses a random position on the board
      *
      * @param input board of the gameState
+     * @return a map of an arbitrary relative value of tiles depending on nearby pieces.
      */
     private float[][] valuemap(int[][] input) {
         float[][] output = new float[input.length][input.length];
@@ -73,42 +78,46 @@ public class GoSmartAI extends GameComputerPlayer {
         for (int x1 = 0; x1 < input.length; x1++) {
             for (int y1 = 0; y1 < input.length; y1++) {
                 if (input[x1][y1] != 0) {
-                    total ++;
+                    total++;
                 }
                 output[x1][y1] = 0;
             }
         }
 
-        for (int x1 = 0; x1 < input.length; x1++) {
-            for (int y1 = 0; y1 < input.length; y1++) {
-                for (int x2 = 0; x2 < input.length; x2++) {
-                    for (int y2 = 0; y2 < input.length; y2++) {
-                        float effect = (float)((Math.abs(y2 - y1) + Math.abs(x2 - x1)));
-                        if (input[x2][y2] == mypeice) {
-                            output[x1][y1] += effect;
-                        } else if (input[x2][y2] != 0){
-                            output[x1][y1] -= effect;
-                        }
-                    }
-                }
+        for (int x = 0; x < input.length; x++) {
+            for (int y = 0; y < input.length; y++) {
+                output[y][x] = 1;
+                if (x != 0 && input[y][x - 1] == 0)
+                    output[y][x]++;
+                if (x != input.length - 1 && input[y][x + 1] == 0)
+                    output[y][x]++;
+                if (y != 0 && input[y - 1][x] == 0)
+                    output[y][x]++;
+                if (y != input.length - 1 && input[y + 1][x] == 0)
+                    output[y][x]++;
+                if (x == 0 || x == input.length - 1)
+                    output[y][x]--;
+                if (y == 0 || y == input.length - 1)
+                    output[y][x]--;
             }
         }
         return output;
     }
 
-    /** choose: chooses the highest value position on the board
+    /**
+     * choose: chooses the highest value position on the board
      *
      * @param input board of the gameState
      */
     private void choose(float[][] input) {
-        Log.d("SmartAI","Choosing...");
+        Log.d("SmartAI", "Choosing...");
         float max = -100000;
         int x_max = -1;
         int y_max = -1;
 
         // Finds the highest value
-        for (int x = 0; x < input.length; x ++) {
-            for (int y = 0; y < input.length; y ++) {
+        for (int x = 0; x < input.length; x++) {
+            for (int y = 0; y < input.length; y++) {
                 if (input[x][y] > max) {
                     max = input[x][y];
                     x_max = x;
@@ -116,9 +125,12 @@ public class GoSmartAI extends GameComputerPlayer {
                 }
             }
         }
-        Log.d("SmartAI","Max: " + max);
-        Log.d("SmartAI","x:" + x_max);
-        Log.d("SmartAI","y:" + y_max);
+
+        Log.d("SmartAI", "Max: " + max);
+        Log.d("SmartAI", "x:" + x_max);
+        Log.d("SmartAI", "y:" + y_max);
+
+        // Decides whether to pass
         if (max > pass) {
             // Updates Move History
             memory[x_max][y_max] = 1;
@@ -126,31 +138,51 @@ public class GoSmartAI extends GameComputerPlayer {
             // Sends GameAction
             game.sendAction(new GoMoveAction(this, x_max, y_max));
         } else {
-            Log.d("SmartAI","Pass");
+            Log.d("SmartAI", "Pass");
             game.sendAction(new GoPassAction(this));
         }
     }
 
+    /**
+     * apply_rng applies a low rng to solve ties
+     *
+     * @param choice the final map to modify
+     */
     private void apply_rng(float[][] choice) {
         Random rand = new Random();
-        for (int x = 0; x < choice.length; x ++) {
-            for (int y = 0; y < choice.length; y ++) {
-                choice[x][y] += (float)rand.nextInt(100) / 200;
+        for (int x = 0; x < choice.length; x++) {
+            for (int y = 0; y < choice.length; y++) {
+                // Generates a random value
+                choice[x][y] += (float) rand.nextInt(100) / 200;
             }
         }
     }
 
+    /**
+     * apply_bias applies a low bias towards center of map
+     *
+     * @param choice the final map to modify
+     */
     private void apply_bias(float[][] choice) {
-        for (int x = 0; x < choice.length; x ++) {
-            for (int y = 0; y < choice.length; y ++) {
-                choice[x][y] += (choice.length - Math.abs((choice.length / 2) + 0.5 - x) - Math.abs((choice.length / 2) + 0.5 - y)) / 300;
+        for (int x = 0; x < choice.length; x++) {
+            for (int y = 0; y < choice.length; y++) {
+                // Generates value based on distance to center
+                choice[x][y] += (choice.length - Math.abs((choice.length / 2) + 0.5 - x) - Math.abs((choice.length / 2) + 0.5 - y)) / choice.length / 10;
             }
         }
     }
 
+    /**
+     * apply_occupied checks if a position is occupied or has been already tried
+     *
+     * @param choice the final map to modify
+     * @param board the game board
+     * @param memory the history of played moves
+     */
     private void apply_occupied(float[][] choice, int[][] board, int[][] memory) {
-        for (int x = 0; x < choice.length; x ++) {
-            for (int y = 0; y < choice.length; y ++) {
+        for (int x = 0; x < choice.length; x++) {
+            for (int y = 0; y < choice.length; y++) {
+                // Checks whether this position has been tried, or a piece already exists
                 if (board[x][y] != 2 || memory[x][y] == 1) {
                     choice[x][y] = -1000000;
                 }
@@ -158,42 +190,106 @@ public class GoSmartAI extends GameComputerPlayer {
         }
     }
 
+    /**
+     * apply_capture attempts to capture pieces
+     *
+     * @param choice the final map to modify
+     * @param value the map of importance of each tile
+     * @param board the game board
+     */
     private void apply_capture(float[][] choice, float[][] value, int[][] board) {
-        for (int x = 0; x < choice.length; x ++) {
-            for (int y = 0; y < choice.length; y ++) {
-                if (board[x][y] == 1) {
-                    if (x != 0) {
-                        choice[x-1][y] += 2;
-                    }
-                    if (x != choice.length - 1) {
-                        choice[x+1][y] += 2;
-                    }
-                    if (y != 0) {
-                        choice[x][y-1] += 2;
-                    }
-                    if (y != choice.length - 1) {
-                        choice[x][y+1] += 2;
+        // Int for calculating freedoms
+        int freedom = 0;
+
+        // Iterates through board
+        for (int x = 0; x < choice.length; x++) {
+            for (int y = 0; y < choice.length; y++) {
+
+                // Calculates freedoms
+                if (board[y][x] == 1) {
+                    freedom = 4;
+                    if (x != 0 && board[y][x - 1] == 0)
+                        freedom--;
+                    if (x != choice.length - 1 && board[y][x + 1] == 0)
+                        freedom--;
+                    if (y != 0 && board[y - 1][x] == 0)
+                        freedom--;
+                    if (y != choice.length - 1 && board[y + 1][x] == 0)
+                        freedom--;
+                    if (x == 0 || x == choice.length - 1)
+                        freedom --;
+                    if (y == 0 || y == choice.length - 1)
+                        freedom --;
+
+                    // Forces AI to be proactive
+                    if (freedom > 1)
+                        freedom --;
+
+                    // Applies weights based on freedom and distance
+                    int row;
+                    for (int col = 0; col < freedom - 1; col++) {
+                        row = freedom - col;
+                        if (y + row < board.length && x + col < board.length)
+                            choice[y + row][x + col] += (4 - freedom) * (4 - freedom) + value[y][x];
+                        if (y - row > 0 && x + col < board.length)
+                            choice[y - row][x + col] += (4 - freedom) * (4 - freedom) + value[y][x];
+                        if (y + row < board.length && x - col > 0)
+                            choice[y + row][x - col] += (4 - freedom) * (4 - freedom) + value[y][x];
+                        if (y - row > 0 && x - col > 0)
+                            choice[y - row][x - col] += (4 - freedom) * (4 - freedom) + value[y][x];
                     }
                 }
             }
         }
     }
 
+    /**
+     * apply_capture attempts to capture pieces
+     *
+     * @param choice the final map to modify
+     * @param value the map of importance of each tile
+     * @param board the game board
+     */
     private void apply_protect(float[][] choice, float[][] value, int[][] board) {
-        for (int x = 0; x < choice.length; x ++) {
-            for (int y = 0; y < choice.length; y ++) {
-                if (board[x][y] == 0) {
-                    if (x != 0) {
-                        choice[x-1][y] += 1;
-                    }
-                    if (x != choice.length - 1) {
-                        choice[x+1][y] += 1;
-                    }
-                    if (y != 0) {
-                        choice[x][y-1] += 1;
-                    }
-                    if (y != choice.length - 1) {
-                        choice[x][y+1] += 1;
+        // Int for calculating freedoms
+        int freedom = 0;
+
+        // Iterates through board
+        for (int x = 0; x < choice.length; x++) {
+            for (int y = 0; y < choice.length; y++) {
+
+                // Calculates freedoms
+                if (board[y][x] == 0) {
+                    freedom = 4;
+                    if (x != 0 && board[y][x - 1] == 1)
+                        freedom--;
+                    if (x != choice.length - 1 && board[y][x + 1] == 1)
+                        freedom--;
+                    if (y != 0 && board[y - 1][x] == 1)
+                        freedom--;
+                    if (y != choice.length - 1 && board[y + 1][x] == 1)
+                        freedom--;
+                    if (x == 0 || x == choice.length - 1)
+                        freedom --;
+                    if (y == 0 || y == choice.length - 1)
+                        freedom --;
+
+                    // Forces AI to be proactive
+                    if (freedom > 1)
+                        freedom --;
+
+                    // Applies weights based on freedom and distance
+                    int row;
+                    for (int col = 0; col < freedom - 1; col++) {
+                        row = freedom - col;
+                        if (y + row < board.length && x + col < board.length)
+                            choice[y + row][x + col] += 2 * ((4 - freedom) * (4 - freedom) + value[y][x]);
+                        if (y - row > 0 && x + col < board.length)
+                            choice[y - row][x + col] += 2 * ((4 - freedom) * (4 - freedom) + value[y][x]);
+                        if (y + row < board.length && x - col > 0)
+                            choice[y + row][x - col] += 2 * ((4 - freedom) * (4 - freedom) + value[y][x]);
+                        if (y - row > 0 && x - col > 0)
+                            choice[y - row][x - col] += 2 * ((4 - freedom) * (4 - freedom) + value[y][x]);
                     }
                 }
             }
